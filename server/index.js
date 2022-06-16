7fd80cefb50bac2cbe2cb0abea5541b2261874a5
LWcaveman <levi_82@hotmail.com>
2022-06-16 14:42:19 -0500

    includes loader io token

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require('newrelic');
+require('dotenv').config(); 
+const path = require('path')
 const express = require('express');
 const app = express();
 const { reviewBuilder  } = require('./controllers/reviews');
 const { metaBuilder  } = require('./controllers/meta');
 const { addAll } = require('./controllers/add');
-require('dotenv').config(); 
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.status(200).send({"This is an API" : "What DID YOUR EXPECT SOMETHING FANCY!"})
@@ -31,5 +33,9 @@ app.post('/reviews/:id', (req, res) => {
   res.status(201).send('Got it!');
 });
 
+//loader io setup
+app.get('/loaderio-d371e994971c8a41bc1a24de8c116d7a/', (req, res) => {
+  res.sendFile('/Users/mstrsplinter/Documents/hackreactor/sdc/public/loaderio-d371e994971c8a41bc1a24de8c116d7a.txt')
+});
 console.log('Listening on PORT: ', process.env.SERVER_PORT);
 app.listen(process.env.SERVER_PORT);
